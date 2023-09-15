194dd50ce9277fb05534fe6746d15cb6612b0939
Luis Oliveira <luisoliveira.jr@hotmail.com>
2023-09-14 23:20:13 -0300

    Update routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsersListComponent } from './components/users-list/users-list.component';
+import { UserViewComponent } from './components/user-view/user-view.component';
+import { UserUpdateComponent } from './components/user-update/user-update.component';
+import { UserNewComponent } from './components/user-new/user-new.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component: UsersListComponent},
+  { path:'app-user-view', component: UserViewComponent},
+  { path:'app-user-new', component: UserNewComponent},
+  { path:'app-user-update', component: UserUpdateComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
