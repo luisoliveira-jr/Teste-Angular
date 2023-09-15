48da135f944da646f9026222640e9a9145862db7
Luis Oliveira <luisoliveira.jr@hotmail.com>
2023-09-14 23:09:13 -0300

    Create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FormComponent } from './components/form/form.component';
+import { NewUserComponent } from './components/new-user/new-user.component';
+import { UpdateUserComponent } from './components/update-user/update-user.component';
+import { UserNewComponent } from './components/user-new/user-new.component';
+import { UserUpdateComponent } from './components/user-update/user-update.component';
+import { UserViewComponent } from './components/user-view/user-view.component';
+import { UsersListComponent } from './components/users-list/users-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    FormComponent,
+    NewUserComponent,
+    UpdateUserComponent,
+    UserNewComponent,
+    UserUpdateComponent,
+    UserViewComponent,
+    UsersListComponent
   ],
   imports: [
     BrowserModule,
